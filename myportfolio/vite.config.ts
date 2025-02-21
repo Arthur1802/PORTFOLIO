0c3a9c23f8d9136cf6e82beb708a5e453ff62154
arthur-qob <arthur.barreto@soulasalle.com.br>
2025-02-21 12:40:24 -0300

    ...

--- myportfolio/vite.config.ts
+++ myportfolio/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-website",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
